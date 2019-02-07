cc6b2cdc55888f30b19948d82f9e49a95a30c89f
Gábor Vecsei <vecseigabor.x@gmail.com>
2019-02-07 19:34:21 +0100

    do not close TF session when calculating complexity

--- filter_pruning/model_complexity_calculation.py
+++ filter_pruning/model_complexity_calculation.py
@@ -5,14 +5,14 @@ from swiss_army_tensorboard import tfboard_loggers
 
 def calculate_flops_and_parameters(model_session):
     run_meta = tf.RunMetadata()
-    with model_session as sess:
-        opts = tf.profiler.ProfileOptionBuilder.float_operation()
-        flops = tf.profiler.profile(sess.graph, run_meta=run_meta, cmd='op', options=opts)
 
-        opts = tf.profiler.ProfileOptionBuilder.trainable_variables_parameter()
-        params = tf.profiler.profile(sess.graph, run_meta=run_meta, cmd='op', options=opts)
+    opts = tf.profiler.ProfileOptionBuilder.float_operation()
+    flops = tf.profiler.profile(model_session.graph, run_meta=run_meta, cmd='op', options=opts)
 
-        return flops.total_float_ops, params.total_parameters
+    opts = tf.profiler.ProfileOptionBuilder.trainable_variables_parameter()
+    params = tf.profiler.profile(model_session.graph, run_meta=run_meta, cmd='op', options=opts)
+
+    return flops.total_float_ops, params.total_parameters
 
 
 class ModelComplexityCallback(callbacks.Callback):
