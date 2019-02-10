a2c5c2078ffc93a0d0a2e8b9f65b09a8084a210c
Gábor Vecsei <vecseigabor.x@gmail.com>
2019-02-10 18:18:37 +0100

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,9 +1,43 @@
+"""
+*****************************************************
+*             Filter Pruning
+*
+*              Gabor Vecsei
+* Website:     https://gaborvecsei.com
+* Blog:        https://gaborvecsei.wordpress.com/
+* LinkedIn:    https://www.linkedin.com/in/gaborvecsei
+* Github:      https://github.com/gaborvecsei
+*
+*****************************************************
+"""
+
+from codecs import open
+from os import path
+
 from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='filter_pruning',
     version='0.0.1',
-    url='gaborvecsei.com',
+    description='Keras model convoltuianl filter pruning',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://gaborvecsei.com',
     author='Gabor Vecsei',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Topic :: Education',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'],
+    keywords='pruning keras vecsei gaborvecsei filter-pruning convolution deep-learning machine-learning',
     packages=find_packages(),
 )
