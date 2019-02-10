2237a02f93993346099d5f945cfcfdb613b2aeab
Gábor Vecsei <vecseigabor.x@gmail.com>
2019-02-10 11:44:28 +0100

    basic setup.py added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='filter_pruning',
+    version='0.0.1',
+    url='gaborvecsei.com',
+    author='Gabor Vecsei',
+    packages=find_packages(),
+)
