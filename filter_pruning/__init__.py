934cc3c7dc023028abe182d22a89a42503240cd8
Gábor Vecsei <vecseigabor.x@gmail.com>
2019-02-15 19:29:17 +0100

    version added

--- filter_pruning/__init__.py
+++ filter_pruning/__init__.py
@@ -1,3 +1,5 @@
 from filter_pruning.base_filter_pruning import BasePruning
 from filter_pruning.kmeans_pruning import KMeansFilterPruning
 from filter_pruning.random_pruning import RandomFilterPruning
+
+__version__ = "0.0.1"
